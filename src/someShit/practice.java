1b336171793dd71ee2e87652f8e8fa344033b386
ericyarek <ericyarek@gmail.com>
2018-06-10 11:52:44 -0400

    new add project

---
+++ src/someShit/practice.java
@@ -0,0 +1,22 @@
+package someShit;
+
+public class practice {
+	public static void main(String[] args) {
+		
+		xyzThere("xyzwoiportdi");
+		
+	}
+		
+
+	
+	public static void xyzThere(String str) {
+		  
+		  
+		for (int i=str.length()-3; i>=0; i--){
+		  System.out.println(str.substring(i, i+3)); {
+		  
+		}
+		}
+	}
+	
+}
